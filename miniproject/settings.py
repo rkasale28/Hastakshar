80616f84b4c82df7118839960cced011557fad60
Dhairya <dhairya.parekh@somaiya.edu>
2021-01-13 19:55:18 +0530

    main app deleted

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
     'register.apps.RegisterConfig',
     'crispy_forms'
 
