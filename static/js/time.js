8c6dc79889a1df959e81bcbd33db4a7e0279cabe
Dhairya <dhairya.parekh@somaiya.edu>
2021-01-23 15:55:48 +0530

    Clock added in Nav

---
+++ static/js/time.js
@@ -0,0 +1,20 @@
+var tday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var tmonth=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+function GetClock(){
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+var nhour=d.getHours(),nmin=d.getMinutes(),ap;
+if(nhour==0){ap=" AM";nhour=12;}
+else if(nhour<12){ap=" AM";}
+else if(nhour==12){ap=" PM";}
+else if(nhour>12){ap=" PM";nhour-=12;}
+
+if(nmin<=9) nmin="0"+nmin;
+
+var clocktext=""+ +""+nhour+":"+nmin+ap+" | "+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
+document.getElementById('clockbox').innerHTML=clocktext;
+}
+
+GetClock();
+setInterval(GetClock,1000);
