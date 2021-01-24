dc1caf9e0743091ba77813a48062f5e908141595
Dhairya <dhairya.parekh@somaiya.edu>
2021-01-24 12:27:02 +0530

    User Preferences fixed

--- static/js/time.js
+++ static/js/time.js
@@ -12,7 +12,7 @@ else if(nhour>12){ap=" PM";nhour-=12;}
 
 if(nmin<=9) nmin="0"+nmin;
 
-var clocktext=""+ +""+nhour+":"+nmin+ap+" | "+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
+var clocktext=""+nhour+":"+nmin+ap+" | "+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
 document.getElementById('clockbox').innerHTML=clocktext;
 }
 
