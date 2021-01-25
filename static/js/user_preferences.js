8cd346a203f82bedcf1842ed37486ef93bdff9ac
Dhairya <dhairya.parekh@somaiya.edu>
2021-01-25 20:15:35 +0530

    Changes implemented in Home, Join Call page

--- static/js/user_preferences.js
+++ static/js/user_preferences.js
@@ -1,77 +1,84 @@
-$(document).ready(function(){
-    var socket = io.connect();
-    
-    let myVideoStream
+$(document).ready(function () {
+  var socket = io.connect();
 
-    const myVideo = document.createElement("video");
-    myVideo.muted = true;
+  let myVideoStream;
 
-    var audio_map = new Map([[true, "Mute"], [false, "Unmute"]])
-    var video_map = new Map([[true, "Stop Video"], [false, "Start Video"]])
+  const myVideo = document.createElement("video");
+  myVideo.muted = true;
 
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-    }).then(stream => {
-        myVideoStream = stream
+  var audio_map = new Map([
+    [true, `<img src="../static/images/mic.png" width="25" height="25">`],
+    [false, `<img src="../static/images/mic_mute.png" width="25" height="25">`],
+  ]);
+  var video_map = new Map([
+    [true, `<img src="../static/images/video.png" width="25" height="25">`],
+    [false, `<img src="../static/images/video-off.png" width="25" height="25">`],
+  ]);
 
-        myVideo.srcObject = myVideoStream
-        
-        myVideo.addEventListener('loadedmetadata', function () {
-            myVideo.play()
-        })
-
-        $('#myVideo').append(myVideo)
-
-        const audio_enabled = myVideoStream.getAudioTracks()[0].enabled;
-        $("#audio").html(audio_map.get(audio_enabled))
-
-        const video_enabled = myVideoStream.getVideoTracks()[0].enabled;
-        $("#video").html(video_map.get(video_enabled))
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
     })
+    .then((stream) => {
+      myVideoStream = stream;
 
-    socket.on('connect', function() {
-        console.log('I\'m connected!');
+      myVideo.srcObject = myVideoStream;
+
+      myVideo.addEventListener("loadedmetadata", function () {
+        myVideo.play();
+      });
+
+      $("#myVideo").append(myVideo);
+
+      const audio_enabled = myVideoStream.getAudioTracks()[0].enabled;
+      $("#audio").html(audio_map.get(audio_enabled));
+
+      const video_enabled = myVideoStream.getVideoTracks()[0].enabled;
+      $("#video").html(video_map.get(video_enabled));
     });
 
-    socket.on('disconnect', function() {
-        console.log('Disconnected');
-    });
+  socket.on("connect", function () {
+    console.log("I'm connected!");
+  });
 
-    socket.on('redirect',function(data){
-        window.location.href = data.url
-    })
+  socket.on("disconnect", function () {
+    console.log("Disconnected");
+  });
 
-    $("#join").click(function(){
-        roomId = $("#roomId").val()
+  socket.on("redirect", function (data) {
+    window.location.href = data.url;
+  });
 
-        if (roomId == "None") roomId = "None"
+  $("#join").click(function () {
+    roomId = $("#roomId").val();
 
-        $.cookie("audio_"+username, myVideoStream.getAudioTracks()[0].enabled);
-        $.cookie("video_"+username, myVideoStream.getVideoTracks()[0].enabled);
-        socket.emit('generate', { roomId: roomId});
-        return false;
-    })
+    if (roomId == "None") roomId = "None";
 
-    $("#audio").click(function () {
-        audio_enabled = myVideoStream.getAudioTracks()[0].enabled
-        myVideoStream.getAudioTracks()[0].enabled = !audio_enabled;
-        $("#audio").html(audio_map.get(!audio_enabled))
-    })
+    $.cookie("audio_" + username, myVideoStream.getAudioTracks()[0].enabled);
+    $.cookie("video_" + username, myVideoStream.getVideoTracks()[0].enabled);
+    socket.emit("generate", { roomId: roomId });
+    return false;
+  });
 
-    $("#video").click(function () {
-        video_enabled = myVideoStream.getVideoTracks()[0].enabled
-        myVideoStream.getVideoTracks()[0].enabled = !video_enabled;
+  $("#audio").click(function () {
+    audio_enabled = myVideoStream.getAudioTracks()[0].enabled;
+    myVideoStream.getAudioTracks()[0].enabled = !audio_enabled;
+    $("#audio").html(audio_map.get(!audio_enabled));
+  });
 
-        $("#video").html(video_map.get(!video_enabled))
+  $("#video").click(function () {
+    video_enabled = myVideoStream.getVideoTracks()[0].enabled;
+    myVideoStream.getVideoTracks()[0].enabled = !video_enabled;
 
-        if (video_enabled){
-            $(".content video").css("display","none");
-            $(".content .overlay").css("display","flex");
-        }
-        else{
-            $(".content video").css("display","flex");
-            $(".content .overlay").css("display","none");
-        }
-    })
-})
+    $("#video").html(video_map.get(!video_enabled));
+
+    if (video_enabled) {
+      $(".content video").css("display", "none");
+      $(".content .overlay").css("display", "flex");
+    } else {
+      $(".content video").css("display", "flex");
+      $(".content .overlay").css("display", "none");
+    }
+  });
+});
