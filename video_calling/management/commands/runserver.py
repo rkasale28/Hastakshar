9a9fb0e31c20474d7b5efa63872fbbb52dcaddd6
rkasale28 <rohitkasale28@gmail.com>
2021-02-06 21:07:22 +0530

    Set Host

--- video_calling/management/commands/runserver.py
+++ video_calling/management/commands/runserver.py
@@ -4,6 +4,7 @@ from video_calling.views import sio
 import os
 
 port = int(os.environ.get("PORT", 8000))
+host = '0.0.0.0'
 
 class Command(RunCommand):
     help = 'Run the Socket.IO server'
@@ -16,7 +17,7 @@ class Command(RunCommand):
             import eventlet
             import eventlet.wsgi
             from miniproject.wsgi import application
-            eventlet.wsgi.server(eventlet.listen(('', port)), application)
+            eventlet.wsgi.server(eventlet.listen((host, port)), application)
         elif sio.async_mode == 'gevent':
             # deploy with gevent
             from gevent import pywsgi
@@ -28,10 +29,10 @@ class Command(RunCommand):
                 websocket = False
             if websocket:
                 pywsgi.WSGIServer(
-                    ('', port), application,
+                    (host, port), application,
                     handler_class=WebSocketHandler).serve_forever()
             else:
-                pywsgi.WSGIServer(('', port), application).serve_forever()
+                pywsgi.WSGIServer((host, port), application).serve_forever()
         elif sio.async_mode == 'gevent_uwsgi':
             print('Start the application through the uwsgi server. Example:')
             print('uwsgi --http :5000 --gevent 1000 --http-websockets '
