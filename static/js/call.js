4cd62ce84224f0a22d4dcff35957584efabf2781
rkasale28 <rohitkasale28@gmail.com>
2021-02-06 14:04:38 +0530

    port

--- static/js/call.js
+++ static/js/call.js
@@ -8,7 +8,7 @@ var video_map = new Map([
 ]);
 const peers = {};
 
-var socket = io.connect();
+const socket = io('/');
 
 var audio_enabled = $.cookie("audio_" + username) === "true";
 var video_enabled = $.cookie("video_" + username) === "true";
--- video_calling/management/commands/runserver.py
+++ video_calling/management/commands/runserver.py
@@ -1,7 +1,9 @@
 from django.core.management.commands.runserver import Command as RunCommand
 
 from video_calling.views import sio
+import os
 
+port = int(os.environ.get("PORT", 8000))
 
 class Command(RunCommand):
     help = 'Run the Socket.IO server'
@@ -14,7 +16,7 @@ class Command(RunCommand):
             import eventlet
             import eventlet.wsgi
             from miniproject.wsgi import application
-            eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
+            eventlet.wsgi.server(eventlet.listen(('', port)), application)
         elif sio.async_mode == 'gevent':
             # deploy with gevent
             from gevent import pywsgi
@@ -26,10 +28,10 @@ class Command(RunCommand):
                 websocket = False
             if websocket:
                 pywsgi.WSGIServer(
-                    ('', 8000), application,
+                    ('', port), application,
                     handler_class=WebSocketHandler).serve_forever()
             else:
-                pywsgi.WSGIServer(('', 8000), application).serve_forever()
+                pywsgi.WSGIServer(('', port), application).serve_forever()
         elif sio.async_mode == 'gevent_uwsgi':
             print('Start the application through the uwsgi server. Example:')
             print('uwsgi --http :5000 --gevent 1000 --http-websockets '
