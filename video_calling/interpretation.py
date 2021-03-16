d92153b87bd5e23651d310daf66b052d36b1492f
rkasale28 <rohitkasale28@gmail.com>
2021-03-16 22:38:16 +0530

    Updates and Installation
    - Updated Tensorflow Folder
      - Removed unnecessary files
      - Added exported model
    - Installed
      - dj-database-url
      - Django
      - django-crispy-forms
      - gevent
      - gevent-websocket
      - greenlet
      - gunicorn
      - Pillow
      - psycopg2
      - python-engineio
      - python-rapidjson
      - python-socketio

--- video_calling/interpretation.py
+++ video_calling/interpretation.py
@@ -1,41 +1,22 @@
-import cv2 
-import numpy as np
-import os
+import tensorflow as tf
+import time
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as viz_utils
-from object_detection.builders import model_builder
-import tensorflow as tf
-from object_detection.utils import config_util
-from object_detection.protos import pipeline_pb2
-from google.protobuf import text_format
-from django.http import JsonResponse
-import time
-import base64
+import numpy as np
 from PIL import Image
-from numpy import asarray
+import base64
 from operator import itemgetter
+from django.http import JsonResponse
 
+PATH_TO_SAVED_MODEL="video_calling/Tensorflow/workspace/training_demo/exported-models/my_model/saved_model"
 WORKSPACE_PATH = 'video_calling/Tensorflow/workspace'
 ANNOTATION_PATH = WORKSPACE_PATH+'/training_demo/annotations'
-MODEL_PATH = WORKSPACE_PATH+'/training_demo/models'
-CONFIG_PATH = MODEL_PATH+'/my_ssd_mobilenet_v2_fpnlite/pipeline.config'
-CHECKPOINT_PATH = MODEL_PATH+'/my_ssd_mobilenet_v2_fpnlite/'
 
-# Load pipeline config and build a detection model
-configs = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
-detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+detect_fn=tf.saved_model.load(PATH_TO_SAVED_MODEL)
+category_index = label_map_util.create_category_index_from_labelmap(ANNOTATION_PATH + '/label_map.pbtxt')
 
-@tf.function
-def detect_fn(image):
-    print('inside detect_fn')
-    image, shapes = detection_model.preprocess(image)
-    print('pre-processing')
-    prediction_dict = detection_model.predict(image, shapes)
-    print('prediction')
-    detections = detection_model.postprocess(prediction_dict, shapes)
-    print('post-processing')
-
-    return detections
+def load_image_into_numpy_array(path):
+    return np.array(Image.open(path))
 
 def interpret(request): 
     print('AJAX Request received')
@@ -47,22 +28,20 @@ def interpret(request):
     with open('static/images/captured_image.jpeg', 'wb') as file_to_save:
         decoded_image_data = base64.decodebytes(base64_img_bytes)
         file_to_save.write(decoded_image_data)
-    print('Saving img')
-	
-    image = Image.open('static/images/captured_image.jpeg')
-    image_np = asarray(image)
-    print('opening img as array')
+    print('Saved img')
 
-    category_index = label_map_util.create_category_index_from_labelmap(ANNOTATION_PATH + '/label_map.pbtxt')
-    
-    # Restore checkpoint
-    ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-    ckpt.restore(os.path.join(CHECKPOINT_PATH, 'ckpt-21')).expect_partial()
-    print('restoring checkpoint')
+    image_np=load_image_into_numpy_array('static/images/captured_image.jpeg')
+    print('Converted to numpy array')
+
+    input_tensor=tf.convert_to_tensor(image_np)
+    print('Converted to tensor')
         
-    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
-    detections = detect_fn(input_tensor)
-    print('completed detection')
+    input_tensor=input_tensor[tf.newaxis, ...]
+    print('Added new axis')
+
+    print ('Starting Detect Fn')
+    detections=detect_fn(input_tensor)
+    print('Detect fn ended')
 
     num_detections = int(detections.pop('num_detections'))
     detections = {key: value[0, :num_detections].numpy()
@@ -92,11 +71,3 @@ def interpret(request):
     }
 
     return JsonResponse(data)
-
-
-# def demo(request):
-#     data = {
-#         "message" : "Hello"
-#     }
-
-#     return JsonResponse(data)
